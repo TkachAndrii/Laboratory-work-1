13d16b66184e913dd15a6b9bd68274e30a7368d4
TkachAndrii <146113089+TkachAndrii@users.noreply.github.com>
2023-12-10 16:39:37 +0200

    Create book.h

---
+++ Library/book.h
@@ -0,0 +1,39 @@
+#include <vector>
+#include <map>
+#include "character.h"
+#include <set>
+#include "../helper.h"
+
+class book {
+    std::string bookName{};
+    std::string annotation{};
+    std::size_t releaseYear{};
+    std::vector<fullName> vectorAuthor{};
+    std::vector<character> vectorCharacter{};
+    std::size_t pageQuantity;
+public:
+    book(std::string &&bookName, std::string &&annotation, std::size_t releaseYear, std::size_t pageQuantity,
+         std::vector<fullName> &&vectorAuthor,
+         std::vector<character> &&vectorCharacter);
+
+    book();
+
+    std::string getName() const;
+
+    std::string getAnnotation() const;
+
+    std::size_t getReleaseYear() const;
+
+    std::size_t getPageQuantity() const;
+
+    std::vector<fullName> getVectorAuthor() const;
+
+    std::vector<character> getVectorCharacter() const;
+
+    friend std::ostream &operator<<(std::ostream &out, const book &bookToPrint);
+
+    bool operator<(const book &rhs) const;
+
+    bool operator==(const book &rhs) const;
+
+};
